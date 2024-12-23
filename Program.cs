a2f39b279de8626087fb6b473f72b601c364c434
Masayoshi-Okubo <m.okubo@infocom.co.jp>
2024-12-23 11:06:39 +0900

    'Commit.'

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("0");
+});
+
+app.Run();
